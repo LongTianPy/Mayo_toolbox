b2c54561736990c000492de9d5f631444b2de64a
LongTianPy <tianlongapp@gmail.com>
2018-05-31 14:46:29 -0500

    add xml file to .gitignore

--- fake_tabix_input.py
+++ fake_tabix_input.py
@@ -28,14 +28,15 @@ if __name__ == '__main__':
     del methyl_chunks
     methyl_chunks = pd.read_table(methyl_file,sep='\t',index_col=0,header=0,engine='python',chunksize=500,iterator=True)
     methyl_df = pd.concat(methyl_chunks)
-    methyl_df["CHROM"] = chrom
-    methyl_df["POS"] = pos
+    methyl_df = methyl_df.insert(loc=0, column='ID', value=list(methyl_df.index))
+    methyl_df = methyl_df.insert(loc=0, column = 'POS',value=pos)
+    methyl_df = methyl_df.insert(loc=0, column='#CHROM', value=chrom)
     cols = list(methyl_df.columns)
     idx_chr = cols.index("CHROM") + 1
     idx_pos = cols.index("POS") + 1
-    methyl_df.index.name = "#ID"
+    methyl_df.index.name = "ID"
     methyl_df = methyl_df.sort_values(by=['CHROM','POS'])
-    methyl_df.to_csv(outfile,sep="\t")
+    methyl_df.to_csv(outfile,sep="\t",index=False)
     # cmd = "(head -n 1 {0} && tail -n +2 {0} | sort -k {1} -k {2}) > sorted_{0}".format(outfile,idx_chr,idx_pos)
     # os.system(cmd)
     os.system("bgzip -c -f {0} > {0}.gz".format(outfile))
