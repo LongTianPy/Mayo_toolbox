8a33f6959b877c929820dd3ba6a7d85ac48579e5
LongTianPy <tianlongapp@gmail.com>
2018-05-31 09:32:54 -0500

    create fake_tabix_input.py

---
+++ fake_tabix_input.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+"""
+"""
+
+# IMPORT
+import pandas as pd
+import sys
+
+# FUNCTIONS
+
+
+# MAIN
+if __name__ == '__main__':
+    probe_annot_file = sys.argv[1]
+    methyl_file = sys.argv[2]
+    probe_annot = pd.read_table(probe_annot_file,sep='\t',index_col=0,header=0,engine='python')
+    methyl_chunks = pd.read_table(methyl_file,sep='\t',index_col=0,header=0,engine='python',chunksize=500,iterator=True)
+    chrom = []
+    pos = []
+    for chunk in methyl_chunks:
+        probes = list(chunk.index)
+        this_chrom = probe_annot.loc[probes,'CHR']
+        this_pos = probe_annot.loc[probes,'MAPINFO']
+        chrom = chrom + this_chrom
+        pos = pos + this_pos
+    del methyl_chunks
+    methyl_chunks = pd.read_table(methyl_file,sep='\t',index_col=0,header=0,engine='python',chunksize=500,iterator=True)
+    methyl_df = pd.concat(methyl_chunks)
+    methyl_df.to_csv('processed_mData.txt')
