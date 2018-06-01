2343ae4a24f2595f0798d42fc05bd36d00a63a68
LongTianPy <tianlongapp@gmail.com>
2018-06-01 15:45:28 -0500

    create multiprocess version

--- process_tabix_input_mp.py
+++ process_tabix_input_mp.py
@@ -12,6 +12,7 @@ import uuid
 num_thread = mp.cpu_count()/4
 probe_annot_file = "/data2/external_data/Sun_Zhifu_zxs01/summerprojects/ltian/probe_notnull.txt"
 probe_annot = pd.read_table(probe_annot_file, sep='\t', index_col=0, header=0, dtype='string')
+outfile = "/data2/external_data/Sun_Zhifu_zxs01/summerprojects/ltian/mData_chunks/mData_chunks.txt"
 
 # FUNCTIONS
 def process_subdf(df):
@@ -23,19 +24,18 @@ def process_subdf(df):
     new.insert(loc=0, column='ID', value=list(df.index))
     new.insert(loc=0, column='POS', value=this_pos)
     new.insert(loc=0, column='#CHROM', value=this_chrom)
+    new.to_csv(outfile+"."+uuid.uuid4(),header=None,index=False,sep="\t")
     return new
 
 
 # MAIN
 if __name__ == '__main__':
     methyl_file = sys.argv[1]
-    outfile = sys.argv[2]
     pool = mp.Pool(num_thread)
     df_list = []
     methyl_chunks = pd.read_table(methyl_file,sep='\t',index_col=0,header=0,engine='python',chunksize=5000,iterator=True)
     for chunk in methyl_chunks:
         df = pool.apply_async(process_subdf,chunk)
-        df.to_csv(outfile+"."+uuid.uuid4(),header=None,index=False,sep="\t")
         df_list.append(df)
     pool.close()
 
