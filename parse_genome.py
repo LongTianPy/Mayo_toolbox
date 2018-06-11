bc47330613fd8f35fb3157d17bb2f699ca12a3e3
LongTianPy <tianlongapp@gmail.com>
2018-06-11 16:41:59 -0500

    load hg19 human genome into database

---
+++ parse_genome.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+"""
+"""
+
+# IMPORT
+import sys
+from MySQLdb import Connect
+
+
+# FUNCTIONS
+def connect_to_db():
+    conn = Connect(host="localhost", user="methyldb",passwd="mayoproject",db="MethylDB")
+    c = conn.cursor()
+    return conn, c
+
+def parse_gff(gff):
+    f = open(gff,"r")
+    lines = [i.strip().split("\t") for i in f.readlines()[9:]]
+    f.close()
+    new_line = []
+    CHR=''
+    for i in lines:
+        if i[2] == 'region':
+            CHR = i[3]
+        elif i[2] == 'gene':
+            start = i[3]
+            end = i[4]
+            info = i[8].split(";")
+            for each in info:
+                if each.startswith("Name="):
+                    gene = each[5:]
+            new_line.append([CHR,start,end,gene])
+        else:
+            continue
+    return new_line
+
+
+# MAIN
+if __name__ == '__main__':
+    gff = sys.argv[1]
+    print(parse_gff(gff))
