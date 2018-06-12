6f9a22ce788e9a73d25f399ec272ad86daed8d3e
LongTianPy <tianlongapp@gmail.com>
2018-06-12 11:52:55 -0500

    create query result parsing solution

--- prepare_boxplot_multi.py
+++ prepare_boxplot_multi.py
@@ -8,9 +8,9 @@ import pandas as pd
 from MySQLdb import Connect
 import uuid
 
-
 acronym_file="/home/long-lamp-username/MethylDB/Acronyms.txt"
 status_file = "/home/long-lamp-username/MethylDB/Tumor_Normal.txt"
+
 # FUNCTIONS
 def connect_to_db():
     conn = Connect(host="localhost", user="methyldb",passwd="mayoproject",db="MethylDB")
@@ -22,24 +22,24 @@ def find_sampleType(sample,c):
     tmp = c.fetchone()
     return tmp
 
-
 def process_data(input):
     # conn,c=connect_to_db()
     df = pd.read_table(input,sep="\t",header=0,index_col=2)
-    df = df.iloc[0,2:]
     with open(acronym_file,"r") as f:
         acronyms = f.read().strip().split(",")
     with open(status_file,"r") as f:
         tumor_normals = f.read().strip().split(",")
+    cpg_ids = list(df.index)
     filename = str(uuid.uuid4())
-    output = '/var/www/html/MethylDB/Result/'+str(filename)+'.txt'
-    return_value = '/MethylDB/Result/'+filename+".txt"
+    output = '/var/www/html/MethylDB/Result/' + str(filename) + '.txt'
+    return_filename = '/MethylDB/Result/' + filename + ".txt"
     with open(output,"w") as f:
-        f.write("Patient,Acronym,TumorNormal,Value\n")
-        for i in range(len(df.index)):
-            line = "{0},{1},{2},{3}\n".format(str(df.index[i]),acronyms[i],tumor_normals[i],df[df.index[i]])
+        f.write("Acronym,TumorNormal," + ",".join(cpg_ids) + "\n")
+        for col in df.columns:
+            values = [str(i) for i in list(df[col])]
+            line = "{0},{1}," + ",".join(values)+"\n"
             f.write(line)
-    return return_value
+    return ",".join(cpg_ids) + "," + return_filename
 
 # MAIN
 if __name__ == '__main__':
