ca09b853cdab7149ee5aac5dd3a044a6d15379d0
LongTianPy <tianlongapp@gmail.com>
2018-06-12 14:03:45 -0500

    create query result parsing solution

--- prepare_boxplot_multi.py
+++ prepare_boxplot_multi.py
@@ -7,6 +7,7 @@ import sys
 import pandas as pd
 from MySQLdb import Connect
 import uuid
+import os
 
 acronym_file="/home/long-lamp-username/MethylDB/Acronyms.txt"
 status_file = "/home/long-lamp-username/MethylDB/Tumor_Normal.txt"
@@ -30,17 +31,36 @@ def process_data(input):
     with open(status_file,"r") as f:
         tumor_normals = f.read().strip().split(",")
     cpg_ids = list(df.index)
-    filename = str(uuid.uuid4())
-    output = '/var/www/html/MethylDB/Result/' + str(filename) + '.txt'
-    return_filename = '/MethylDB/Result/' + filename + ".txt"
-    with open(output,"w") as f:
-        f.write("Acronym,TumorNormal," + ",".join(cpg_ids) + "\n")
-        for i in range(len(df.columns[2:])):
-            col = df.columns[2:][i]
-            values = [str(j) for j in list(df[col])]
-            line = "{0},{1},".format(acronyms[i],tumor_normals[i]) + ",".join(values)+"\n"
-            f.write(line)
+    # filename = str(uuid.uuid4())
+    output = '/var/www/html/MethylDB/Result/{0}.txt'
+    # return_filename = '/MethylDB/Result/{0}.txt'
+    # with open(output,"w") as f:
+    #     f.write("Acronym,TumorNormal," + ",".join(cpg_ids) + "\n")
+    #     for i in range(len(df.columns[2:])):
+    #         col = df.columns[2:][i]
+    #         values = [str(j) for j in list(df[col])]
+    #         line = "{0},{1},".format(acronyms[i],tumor_normals[i]) + ",".join(values)+"\n"
+    #         f.write(line)
+    for cpg_id in cpg_ids:
+        if not os.path.isfile(output.format(cpg_id)):
+            with open(output.format(cpg_id),"w") as f:
+                f.write("Acronym,TumorNormal,Value\n")
+                values = df.loc[cpg_id,df.columns[2:]]
+                for i in range(len(acronyms)):
+                    line = "{0},{1},{2}\n".format(acronyms[i],tumor_normals[i],str(values[i]))
+                    f.write(line)
+    return ",".join(cpg_ids)
+
+
+
+
+
+
+
     return ",".join(cpg_ids) + "," + return_filename
+
+
+
 
 # MAIN
 if __name__ == '__main__':
