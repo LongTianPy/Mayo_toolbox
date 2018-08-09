d86221cf087ae0517cca168c028fd0d9707315fb
LongTianPy <tianlongapp@gmail.com>
2018-08-09 09:54:36 -0500

    re-organize CpGs by CpG Islands

--- group_CpG_by_island.py
+++ group_CpG_by_island.py
@@ -15,31 +15,26 @@ cpg_result = "/data2/external_data/Sun_Zhifu_zxs01/summerprojects/ltian/MethylDB
 # FUNCTIONS
 def create_island_id(cpg_table):
     df = pd.read_table(cpg_table,sep="\t",header=0,index_col=None,dtype='str')
-    to_remove = []
-    for i in df.index:
-        cpg_id = df.loc[i,"Probeset_ID"]
-        if not isfile(cpg_result+cpg_id+".txt"):
-            to_remove.append(i)
-    df.drop(df.index[to_remove])
     cpg_islands = {}
     for i in df.index:
         cpg = df.loc[i,"Probeset_ID"]
-        island_name = df.loc[i,"UCSC_CpG_Islands_Name"]
-        island_relation = df.loc[i,"Relation_to_UCSC_CpG_Island"]
-        if island_name not in cpg_islands:
-            cpg_islands[island_name] = {island_relation:[cpg]}
-        else:
-            if island_relation not in cpg_islands[island_name]:
-                cpg_islands[island_name][island_relation] = [cpg]
+        if isfile(cpg_result+cpg + ".txt"):
+            island_name = df.loc[i,"UCSC_CpG_Islands_Name"]
+            island_relation = df.loc[i,"Relation_to_UCSC_CpG_Island"]
+            if island_name not in cpg_islands:
+                cpg_islands[island_name] = {island_relation:[cpg]}
             else:
-                cpg_islands[island_name][island_relation].append(cpg)
+                if island_relation not in cpg_islands[island_name]:
+                    cpg_islands[island_name][island_relation] = [cpg]
+                else:
+                    cpg_islands[island_name][island_relation].append(cpg)
     island_count = 0
     island_id_list = []
     island_name_list = []
     island_relation_list = []
     island_members_list = []
     new_df = pd.DataFrame()
-    for i in cpg_islands:
+    for i in cpg_islands.keys():
         for j in cpg_islands[i]:
             island_id = "Island_{0}".format(island_count)
             island_count += 1
@@ -65,12 +60,20 @@ def reorganize_data(datafile):
     df = pd.read_table(datafile,sep="\t",header=0,index_col=1)
     new_df = pd.DataFrame(0,index=df.index,columns=group_df.index)
     for idx in new_df.index:
+        # print(idx)
         for each_island_id in group_df.index:
-            sub_df = df.loc[idx,group_df.loc[each_island_id,"Island_Members"].split(",")]
-            ave = sub_df.mean(numeric_only=True)
+            # print(each_island_id)
+            cols = group_df.loc[each_island_id,"Island_Members"].split(",")
+            if len(cols)>1:
+                cols = cols
+            else:
+                cols = cols[0]
+            sub_df = df.loc[idx,cols]
+            ave = sub_df.mean()
             new_df.loc[idx,each_island_id]=ave
     new_df.to_csv(base_dir + datafile + ".reorganized",sep="\t")
 
 # MAIN
 if __name__ == '__main__':
-    reorganize_data(base_dir+sys.argv[1])
+    create_island_id(cpg_table)
+    # reorganize_data(base_dir+sys.argv[1])
